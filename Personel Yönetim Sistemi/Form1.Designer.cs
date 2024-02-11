9c3140e9e0f4baff18f58653d5fe98705554c172
bahadır <bahadir.aslanb20@gmail.com>
2024-02-12 00:41:30 +0300

    Proje dosyası ekle.

---
+++ Personel Yönetim Sistemi/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Personel_Yönetim_Sistemi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.btnGiris = new System.Windows.Forms.Button();
+            this.chkSifreyiGosterGizle = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Şifrenizi Giriniz";
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSifre.Location = new System.Drawing.Point(173, 94);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.PasswordChar = '*';
+            this.txtSifre.Size = new System.Drawing.Size(246, 26);
+            this.txtSifre.TabIndex = 1;
+            this.txtSifre.TextChanged += new System.EventHandler(this.txtSifre_TextChanged);
+            // 
+            // btnGiris
+            // 
+            this.btnGiris.Location = new System.Drawing.Point(227, 126);
+            this.btnGiris.Name = "btnGiris";
+            this.btnGiris.Size = new System.Drawing.Size(131, 52);
+            this.btnGiris.TabIndex = 2;
+            this.btnGiris.Text = "Giriş Yap";
+            this.btnGiris.UseVisualStyleBackColor = true;
+            this.btnGiris.Click += new System.EventHandler(this.btnGiris_Click);
+            // 
+            // chkSifreyiGosterGizle
+            // 
+            this.chkSifreyiGosterGizle.AutoSize = true;
+            this.chkSifreyiGosterGizle.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.chkSifreyiGosterGizle.Location = new System.Drawing.Point(425, 97);
+            this.chkSifreyiGosterGizle.Name = "chkSifreyiGosterGizle";
+            this.chkSifreyiGosterGizle.Size = new System.Drawing.Size(111, 23);
+            this.chkSifreyiGosterGizle.TabIndex = 3;
+            this.chkSifreyiGosterGizle.Text = "Şifreyi Göster";
+            this.chkSifreyiGosterGizle.UseVisualStyleBackColor = true;
+            this.chkSifreyiGosterGizle.CheckedChanged += new System.EventHandler(this.chkSifreyiGosterGizle_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(543, 238);
+            this.Controls.Add(this.chkSifreyiGosterGizle);
+            this.Controls.Add(this.btnGiris);
+            this.Controls.Add(this.txtSifre);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Giriş";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.Button btnGiris;
+        private System.Windows.Forms.CheckBox chkSifreyiGosterGizle;
+    }
+}
+
---
+++ Personel Yönetim Sistemi/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Personel_Yönetim_Sistemi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chkSifreyiGosterGizle_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkSifreyiGosterGizle.Checked)
+            {
+                txtSifre.PasswordChar = '\0'; // Şifreyi göster
+            }
+            else
+            {
+                txtSifre.PasswordChar = '*'; // Şifreyi gizle
+            }
+        }
+
+        private void txtSifre_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void btnGiris_Click(object sender, EventArgs e)
+        {
+            // Girilen şifreyi kontrol et ve doğruysa Form2'yi aç
+            if (txtSifre.Text == "isyeri123")
+            {
+                Form2 form2 = new Form2();
+                form2.Show();
+                this.Hide(); // Form1'i gizle
+            }
+            else
+            {
+                MessageBox.Show("Hatalı şifre! Lütfen tekrar deneyin.");
+            }
+        }
+    }
+}
+
+
